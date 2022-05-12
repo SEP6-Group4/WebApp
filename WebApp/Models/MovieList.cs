5adcc72abac0300783e02b1b710c9e315dec7c2f
Kianoush97 <58509806+Kianoush97@users.noreply.github.com>
2022-05-12 10:30:18 +0200

    Shows the list of movies

---
+++ WebApp/Models/MovieList.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models
+{
+    public class MovieList
+    {
+        [JsonPropertyName("page")]
+        public int CurrentPage { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<Movie> movies { get; set; }
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPage { get; set; }
+    }
+}
