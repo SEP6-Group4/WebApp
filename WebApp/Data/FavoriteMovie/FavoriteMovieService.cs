08d53a3f77991cc655ddbb09ea8b17ee54e988bb
Christian Sejer <christian_sejer@hotmail.com>
2022-05-17 16:57:43 +0200

    added movie favoriting

---
+++ WebApp/Data/FavoriteMovie/FavoriteMovieService.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Text.Json;
+using WebApp.Models;
+
+namespace WebApp.Data.FavoriteMovie
+{
+    public class FavoriteMovieService : IFavoriteMovieService
+    {
+#if DEBUG
+        string url = "https://localhost:7176/api/FavoriteMovie";
+#else
+       
+        string url = "http://moviewebapi-dev.us-east-1.elasticbeanstalk.com/api/FavoriteMovie";
+#endif
+        HttpClient client;
+
+        public FavoriteMovieService()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task AddFavoriteMovie(int userID, int movieID)
+        {
+            HttpContent content = new StringContent(
+                "",
+                Encoding.UTF8,
+                "application/json"
+                );
+            await client.PostAsync(url + "/addFavorite?userID=" + userID + "&movieID=" + movieID, content);
+        }
+
+        public async Task<MovieList> GetFavoriteMoviesByID(int userID)
+        {
+            string message = await client.GetStringAsync(url + "/getFavorites?userID=" + userID);
+            MovieList results = JsonSerializer.Deserialize<MovieList>(message);
+            return results;
+        }
+
+        public async Task<bool> GetIsFavoriteMovieByID(int userID, int movieID)
+        {
+            string message = await client.GetStringAsync(url + "/getFavorite?userID=" + userID + "&movieID=" + movieID);
+            bool result = JsonSerializer.Deserialize<bool>(message);
+            return result;
+        }
+
+        public async Task RemoveFavoriteMovieByID(int userID, int movieID)
+        {
+            await client.DeleteAsync(url + "/removeFavorite?userID=" + userID + "&movieID=" + movieID);
+        }
+    }
+}
