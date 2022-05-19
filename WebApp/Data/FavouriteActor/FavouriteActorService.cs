fce1ad43ea3fe1e4fc0fb12b4c827147e2cbcdcf
Kianoush97 <58509806+Kianoush97@users.noreply.github.com>
2022-05-19 10:41:34 +0200

    Adds and removes Actor from favourite list

---
+++ WebApp/Data/FavouriteActor/FavouriteActorService.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace WebApp.Data.FavouriteActor
+{
+    public class FavouriteActorService : IFavouriteActorService
+    {
+#if DEBUG
+        string url = "https://localhost:7176/FavouriteActor";
+#else
+       
+        string url = "http://moviewebapi-dev.us-east-1.elasticbeanstalk.com/FavouriteActor";
+#endif
+
+        HttpClient client;
+
+        public FavouriteActorService()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task AddActorToFavourite(int userId, int actorId)
+        {
+            HttpContent content = new StringContent("", Encoding.UTF8, "application/json");
+
+            await client.PostAsync(url + "/AddActorToFavourite/" + userId + "/" + actorId, content);
+        }
+
+        public async Task RemoveActorFromFavourite(int userId, int actorId)
+        {
+            await client.DeleteAsync(url + "/RemoveActorFromFavourite/" + userId + "/" + actorId);
+        }
+
+        public async Task<List<int>> GetFavouriteActorIds(int userId)
+        {
+            string message = await client.GetStringAsync(url + "/GetFavouriteActorIds/" + userId);
+            List<int> result = JsonSerializer.Deserialize<List<int>>(message);
+            return result;
+        }
+
+    }
+}
---
+++ WebApp/Data/FavouriteActor/IFavouriteActorService.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Data.FavouriteActor
+{
+    public interface IFavouriteActorService
+    {
+        Task AddActorToFavourite(int userId, int actorId);
+
+        Task RemoveActorFromFavourite(int userId, int actorId);
+
+        Task<List<int>> GetFavouriteActorIds(int userId);
+    }
+}
