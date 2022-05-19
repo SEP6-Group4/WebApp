af768be4a65d0ef3850b9ccd72465a36ff394027
Christian Sejer <christian_sejer@hotmail.com>
2022-05-19 11:16:06 +0200

    fixed the remote url for userservice

--- WebApp/Data/Users/UserService.cs
+++ WebApp/Data/Users/UserService.cs
@@ -10,7 +10,7 @@ namespace WebApp.Data.Users
         string url = "https://localhost:7176/api/user";
 #else
        
-        string url = "http://moviewebapi-dev.us-east-1.elasticbeanstalk.com/user";
+        string url = "http://moviewebapi-dev.us-east-1.elasticbeanstalk.com/api/user";
 #endif
 
 
